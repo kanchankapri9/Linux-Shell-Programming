ef82a8901c05a538f15b0a30b9ef44348628de6b
kanchan kapri <kaprikanchan09@gmail.com>
2025-11-19 18:51:37 +0530

    Personalize appreciation message in demo.sh

--- Lab_Question/demo.sh
+++ Lab_Question/demo.sh
@@ -2,12 +2,13 @@ echo "Enter Your Name"
 read name
 
 echo "kal hamara exam hai 20-11-25 ko isliye ye folder bana hai bye"
-
-
-echo "ENTER THE NUMBER THAT , how many times you wants to hear your appritiation"
+echo ""
+echo "ENTER THE NUMBER THAT , how many times you wanna hear your appritiation"
 read n
 
 for (( i=0; i<n; i++ )) do
-echo "You are the best"
+echo " $name You are the best"
 done
+
+read -p  "hit the keyboard at anywhere and leave"
 
