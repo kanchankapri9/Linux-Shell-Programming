e5833b15ac3d9a21065878147b464beb13c8feb1
kanchan kapri <kaprikanchan09@gmail.com>
2025-11-19 12:46:14 +0000

    made on fun demo file

---
+++ Lab_Question/demo.sh
@@ -0,0 +1,13 @@
+echo "Enter Your Name" 
+read name
+
+echo "kal hamara exam hai 20-11-25 ko isliye ye folder bana hai bye"
+
+
+echo "ENTER THE NUMBER THAT , how many times you wants to hear your appritiation"
+read n
+
+for (( i=0; i<n; i++ )) do
+echo "You are the best"
+done
+
